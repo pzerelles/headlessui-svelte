7df10f4f749b94cea9b943ce4f0b97dfb09fbcf0
Philipp Zerelles <philipp@zerelles.com>
2024-06-06 19:26:33 +0200

    feat: add Checkbox, Description, Field, Fieldset, Label and Legend components

---
+++ .changeset/commit.cjs
@@ -0,0 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const outdent = require("outdent")
+
+/**
+ * @type {NonNullable<import('@changesets/types').CommitFunctions["getAddMessage"]>}
+ */
+const getAddMessage = async (changeset, options) => {
+  const skipCI = options?.skipCI === "add" || options?.skipCI === true
+  return outdent`${changeset.summary}${skipCI ? `\n\n[skip ci]\n` : ""}`
+}
+
+/**
+ * @type {NonNullable<import('@changesets/types').CommitFunctions["getVersionMessage"]>}
+ */
+const getVersionMessage = async (releasePlan, options) => {
+  const skipCI = options?.skipCI === "version" || options?.skipCI === true
+  const publishableReleases = releasePlan.releases.filter((release) => release.type !== "none")
+  const numPackagesReleased = publishableReleases.length
+
+  const releasesLines = publishableReleases.map((release) => `  ${release.name}@${release.newVersion}`).join("\n")
+
+  return outdent`
+    RELEASING: Releasing ${numPackagesReleased} package(s)
+
+    Releases:
+    ${releasesLines}
+    ${skipCI ? `\n[skip ci]\n` : ""}
+`
+}
+
+exports.default = {
+  getAddMessage,
+  getVersionMessage,
+}
---
+++ .changeset/tidy-singers-think.md
@@ -0,0 +1,5 @@
+---
+"@pzerelles/headlessui-svelte": minor
+---
+
+feat: add Checkbox, Description, Field, Fieldset, Label and Legend components
