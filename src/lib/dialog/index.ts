6b47e6bc1fb28ce4d19fa168c04c2cea43f3c286
Philipp Zerelles <philipp@zerelles.com>
2024-08-20 10:50:40 +0200

    fix: DialogBackdrop export

--- src/lib/dialog/index.ts
+++ src/lib/dialog/index.ts
@@ -1,4 +1,4 @@
 export { default as Dialog, type DialogProps } from "./Dialog.svelte"
-export { default as DialogBackgrop, type DialogBackdropProps } from "./DialogBackdrop.svelte"
+export { default as DialogBackdrop, type DialogBackdropProps } from "./DialogBackdrop.svelte"
 export { default as DialogPanel, type DialogPanelProps } from "./DialogPanel.svelte"
 export { default as DialogTitle, type DialogTitleProps } from "./DialogTitle.svelte"
